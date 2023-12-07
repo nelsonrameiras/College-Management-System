cf82f9c0fe78e5a96d31e9b7319b70508c973053
Nelson Ramos <nelsonramos@pm.me>
2023-12-07 19:00:50 +0000

    Integrated DB, so as to be published and used in the overall Workflow. Also added Readme.

---
+++ CollegeDB/CollegeDB/dbo/Tables/NewAdmission.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[NewAdmission] (
+    [NAID]                INT           IDENTITY (1, 1) NOT NULL,
+    [fullName]            VARCHAR (250) NOT NULL,
+    [motherName]          VARCHAR (250) NOT NULL,
+    [gender]              VARCHAR (10)  NOT NULL,
+    [dateOfBirth]         VARCHAR (50)  NOT NULL,
+    [mobileNumber]        BIGINT        NOT NULL,
+    [email]               VARCHAR (150) NOT NULL,
+    [semester]            VARCHAR (150) NOT NULL,
+    [programmingLanguage] VARCHAR (150) NOT NULL,
+    [schoolName]          VARCHAR (150) NOT NULL,
+    [duration]            VARCHAR (120) NOT NULL,
+    [addresss]            VARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([NAID] ASC)
+);
+
---
+++ README.txt
@@ -0,0 +1,6 @@
+Please, publish the database, copy the connection string and paste it in the app.config file in the CollegeManagementSystem Project.
+Otherwise, some features will not be available.
+
+If you have any doubts, I will be commenting the major steps in the development of this project.
+
+Enjoy!
