d0b6fe9cb75e4eeefdda91033cade6f774770b74
Nelson Ramos <nelsonramos@pm.me>
2023-12-28 15:23:42 +0000

    Added functionality to Search Student DGV.

--- CollegeManagementSystem/FormSearchStudent.Designer.cs
+++ CollegeManagementSystem/FormSearchStudent.Designer.cs
@@ -35,6 +35,8 @@
             this.registrationNumberTextBox = new System.Windows.Forms.TextBox();
             this.searchStudentsButton = new System.Windows.Forms.Button();
             this.studentsDataGridView = new System.Windows.Forms.DataGridView();
+            this.searchAllRadioButton = new System.Windows.Forms.RadioButton();
+            this.searchOnlyWithFeesRadioButton = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.userPictureBox)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.studentsDataGridView)).BeginInit();
             this.SuspendLayout();
@@ -79,7 +81,7 @@
             // searchStudentsButton
             // 
             this.searchStudentsButton.BackColor = System.Drawing.Color.DodgerBlue;
-            this.searchStudentsButton.Location = new System.Drawing.Point(350, 164);
+            this.searchStudentsButton.Location = new System.Drawing.Point(390, 168);
             this.searchStudentsButton.Name = "searchStudentsButton";
             this.searchStudentsButton.Size = new System.Drawing.Size(99, 37);
             this.searchStudentsButton.TabIndex = 4;
@@ -98,12 +100,36 @@
             this.studentsDataGridView.Size = new System.Drawing.Size(588, 254);
             this.studentsDataGridView.TabIndex = 5;
             // 
+            // searchAllRadioButton
+            // 
+            this.searchAllRadioButton.AutoSize = true;
+            this.searchAllRadioButton.Location = new System.Drawing.Point(192, 147);
+            this.searchAllRadioButton.Name = "searchAllRadioButton";
+            this.searchAllRadioButton.Size = new System.Drawing.Size(48, 26);
+            this.searchAllRadioButton.TabIndex = 6;
+            this.searchAllRadioButton.TabStop = true;
+            this.searchAllRadioButton.Text = "All";
+            this.searchAllRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // searchOnlyWithFeesRadioButton
+            // 
+            this.searchOnlyWithFeesRadioButton.AutoSize = true;
+            this.searchOnlyWithFeesRadioButton.Location = new System.Drawing.Point(192, 179);
+            this.searchOnlyWithFeesRadioButton.Name = "searchOnlyWithFeesRadioButton";
+            this.searchOnlyWithFeesRadioButton.Size = new System.Drawing.Size(147, 26);
+            this.searchOnlyWithFeesRadioButton.TabIndex = 7;
+            this.searchOnlyWithFeesRadioButton.TabStop = true;
+            this.searchOnlyWithFeesRadioButton.Text = "Only with Fees";
+            this.searchOnlyWithFeesRadioButton.UseVisualStyleBackColor = true;
+            // 
             // FormSearchStudent
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 22F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(654, 511);
+            this.Controls.Add(this.searchOnlyWithFeesRadioButton);
+            this.Controls.Add(this.searchAllRadioButton);
             this.Controls.Add(this.studentsDataGridView);
             this.Controls.Add(this.searchStudentsButton);
             this.Controls.Add(this.registrationNumberTextBox);
@@ -132,5 +158,7 @@
         private System.Windows.Forms.TextBox registrationNumberTextBox;
         private System.Windows.Forms.Button searchStudentsButton;
         private System.Windows.Forms.DataGridView studentsDataGridView;
+        private System.Windows.Forms.RadioButton searchAllRadioButton;
+        private System.Windows.Forms.RadioButton searchOnlyWithFeesRadioButton;
     }
 }
--- CollegeManagementSystem/FormSearchStudent.cs
+++ CollegeManagementSystem/FormSearchStudent.cs
@@ -40,29 +40,68 @@ namespace CollegeManagementSystem
 
         private void searchStudentsButton_Click(object sender, EventArgs e)
         {
-            if (registrationNumberTextBox.Text == "")
+            bool allIsChecked = searchAllRadioButton.Checked;
+            bool wFeesIsChecked = searchOnlyWithFeesRadioButton.Checked;
+
+            SqlConnection cnn = new SqlConnection();
+            cnn.ConnectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = cnn;
+
+            if (registrationNumberTextBox.Text == "" && (!allIsChecked && !wFeesIsChecked))
             {
-                MessageBox.Show("Please Enter a Registration Number. Otherwise, the query cannot procede.", "Unsuccessful.",
+                MessageBox.Show("Please Enter a Registration Number or select an option. Otherwise, that specific query cannot procede.", "Unsuccessful.",
                     MessageBoxButtons.OK, MessageBoxIcon.Error);
                 FormSearchStudent_Load(sender, e);
                 studentsDataGridView.ReadOnly = true;
             }
-            else
+            else if (((allIsChecked || (!allIsChecked && !wFeesIsChecked)) && registrationNumberTextBox.Text != ""))
             {
-                SqlConnection cnn = new SqlConnection();
-                cnn.ConnectionString = ConfigurationManager.ConnectionStrings["Database"].ConnectionString;
-                SqlCommand cmd = new SqlCommand();
-                cmd.Connection = cnn;
-
-                cmd.CommandText = $"SELECT * FROM NewAdmission where NAID = {registrationNumberTextBox.Text}";
+                cmd.CommandText = $"SELECT * From NewAdmission WHERE NAID = {registrationNumberTextBox.Text}";
 
                 SqlDataAdapter adapter = new SqlDataAdapter(cmd);
                 DataSet dataset = new DataSet();
                 adapter.Fill(dataset);
 
                 studentsDataGridView.DataSource = dataset.Tables[0];
-                studentsDataGridView.ReadOnly = true;
             }
-        }
+            else if (((allIsChecked || (!allIsChecked && !wFeesIsChecked)) && registrationNumberTextBox.Text == ""))
+            {
+                cmd.CommandText = "SELECT * From NewAdmission";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet dataset = new DataSet();
+                adapter.Fill(dataset);
+
+                studentsDataGridView.DataSource = dataset.Tables[0];
+            }
+            else if (((wFeesIsChecked) && registrationNumberTextBox.Text != ""))
+            {
+                cmd.CommandText = $"SELECT * From NewAdmission WHERE NAID = {registrationNumberTextBox.Text}";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet dataset = new DataSet();
+                adapter.Fill(dataset);
+
+                studentsDataGridView.DataSource = dataset.Tables[0];
+            }
+            else if (((wFeesIsChecked) && registrationNumberTextBox.Text == ""))
+            {
+                cmd.CommandText = $"SELECT NewAdmission.NAID, NewAdmission.fullName, NewAdmission.motherName," +
+                               $"NewAdmission.gender, NewAdmission.dateOfBirth, NewAdmission.mobileNumber, NewAdmission.email," +
+                               $"NewAdmission.semester, NewAdmission.programmingLanguage, NewAdmission.schoolName," +
+                               $"NewAdmission.duration, NewAdmission.addresss, Fees.fees FROM NewAdmission INNER JOIN Fees " +
+                               $"ON NewAdmission.NAID = Fees.NAID";
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet dataset = new DataSet();
+                adapter.Fill(dataset);
+
+                studentsDataGridView.DataSource = dataset.Tables[0];
+            }
+                
+            studentsDataGridView.ReadOnly = true;
+
+            }
     }
 }
